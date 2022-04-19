df662cb71b9244a7bfd4e680b64d41f338259bcd
임도균 <im.koremp@gmail.com>
2022-04-19 16:36:10 +0900

    Add comment to boj/class2/1654.py

--- boj/class2/1654.py
+++ boj/class2/1654.py
@@ -21,4 +21,7 @@ while (start <= end):
         result = mid
         start = mid + 1
 
- print(result)
+print(result)
+# print(result) 앞에 빈칸이 하나 있었는데 백준의 코드 작성 환경에서는 에러를 띄워주지 않았다.
+# boj 관련 chrome extension이나 vscode extension을 작성하는 방법을 생각해보자
+# vscode extension은 애매한 것 같으니 terminal 프로그램으로 제출하는 방법도 괜찮을 것 같다.
