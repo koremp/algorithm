358aed84e953722f1fcf06f51407f9876948a754
Dokyun Claire Lim <im.koremp@gmail.com>
2024-04-25 20:04:11 +0900

    add 1074

--- boj/class3/1074.py
+++ boj/class3/1074.py
@@ -2,5 +2,23 @@
 
 from sys import stdin
 
+def dim(n, r, c, count):
+    if n <= 0:
+        return count
+
+    divide = (2 ** (n - 1)) ** 2
+
+    if r < 2 ** (n - 1):
+        if c < 2 ** (n - 1):
+            return dim(n - 1, r, c, count)
+        else:
+            return dim(n - 1, r, c - (2 ** (n - 1)), count + divide)
+    else:
+        if c < 2 ** (n - 1):
+            return dim(n - 1, r - (2 ** (n - 1)), c, count + divide * 2)
+        else:
+            return dim(n - 1, r - (2 ** (n - 1)), c - (2 ** (n - 1)), count + divide * 3)
+
 n, r, c = map(int, stdin.readline().rstrip().split())
 
+print(dim(n = n, r = r, c = c, count = 0))
