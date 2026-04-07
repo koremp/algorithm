d534664b9b1bf191bf79a11a2a3dc503fee1475e
Dokyun Claire Lim <im.koremp@gmail.com>
2026-04-07 14:53:30 +0900

    add 11726

---
+++ boj/class3/11726.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+dp = [0] * 1001
+dp[0] = 0
+dp[1] = 1
+dp[2] = 2
+dp[3] = 3
+
+for i in range(4, 1001):
+  dp[i] = dp[i-1] + dp[i-2]
+
+
+print(dp[int(stdin.readline())] % 10007)
